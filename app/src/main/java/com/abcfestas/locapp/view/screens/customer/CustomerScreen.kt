69b81768e2a107dfd194e6575ff82dca89ea7b50
Bruno Roboredo <vital.bcroboredo@terceiros.mag.com.br>
2024-06-08 17:22:46 +0100

    added search and pagination on customers list

--- app/src/main/java/com/abcfestas/locapp/view/screens/customer/CustomerScreen.kt
+++ app/src/main/java/com/abcfestas/locapp/view/screens/customer/CustomerScreen.kt
@@ -14,13 +14,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -41,12 +53,12 @@ import com.abcfestas.locapp.view.components.Button
 import com.abcfestas.locapp.view.navigation.ScreensEnum
 import com.abcfestas.locapp.viewmodel.customer.CustomerViewModel
 import com.abcfestas.locapp.viewmodel.viewModelFactory
+import kotlinx.coroutines.launch
 
 @Composable
 fun CustomerScreen(
     navController: NavController
 ) {
-
     // TODO use LayoutDefault - remove modifier.scroll from layoutdefault
     Column(
         modifier = Modifier
@@ -56,7 +68,7 @@ fun CustomerScreen(
     ) {
         Column(modifier = Modifier.weight(1f, false)) {
             Text(text = stringResource(R.string.customers), style = Typography.titleLarge)
-            Spacer(modifier = Modifier.height(16.dp))
+            // Spacer(modifier = Modifier.height(16.dp))
 
             ListCustomers()
         }
@@ -71,6 +83,7 @@ fun CustomerScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListCustomers(
     viewModel: CustomerViewModel = viewModel(factory = viewModelFactory {
@@ -79,16 +92,63 @@ fun ListCustomers(
 ) {
     val customerList by remember { viewModel.customers }
     val loading by remember { viewModel.loading }
+    var hasNextPage by remember { viewModel.hasNextPage }
+    var currentPage by remember { viewModel.currentPage }
     val error by remember { viewModel.error }
+    var search by remember { viewModel.search }
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    // search
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         viewModel.loadCustomers()
     }
 
-    LazyColumn {
+    LaunchedEffect(listState) {
+        snapshotFlow { listState.layoutInfo.visibleItemsInfo }
+            .collect { visibleItems ->
+                if (
+                    hasNextPage &&
+                    visibleItems.isNotEmpty() &&
+                    visibleItems.last().index == customerList.lastIndex
+                ) {
+                    coroutineScope.launch {
+                        currentPage++
+                        viewModel.loadCustomers()
+                    }
+                }
+            }
+    }
+
+    SearchBar(
+        modifier = Modifier,
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = search,
+                onQueryChange = {
+                    search = it
+                    viewModel.loadCustomers()
+                },
+                onSearch = {
+                    expanded = false
+                    viewModel.loadCustomers()
+                },
+                expanded = expanded,
+                onExpandedChange = { expanded = it },
+                placeholder = { Text(stringResource(id = R.string.customer_search_placeholder)) },
+                leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+            )
+        },
+        expanded = false,
+        onExpandedChange = { expanded = it },
+        content = {}
+    )
+    Spacer(modifier = Modifier.height(24.dp))
+
+    LazyColumn(state = listState) {
         items(customerList) {
-            // TODO: pagination
-            // viewModel.loadCustomers()
             CustomerBox(customer =  it)
         }
     }
--- app/src/main/java/com/abcfestas/locapp/viewmodel/customer/CustomerViewModel.kt
+++ app/src/main/java/com/abcfestas/locapp/viewmodel/customer/CustomerViewModel.kt
@@ -1,6 +1,7 @@
 package com.abcfestas.locapp.viewmodel.customer
 
 import android.util.Log
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,22 +14,41 @@ class CustomerViewModel (
     private val repository: CustomerRepository
 ): ViewModel() {
 
-    private var currentPage = 1;
-
+    var currentPage = mutableIntStateOf(1)
     var customers = mutableStateOf<List<Customer>>(listOf())
     var error = mutableStateOf("")
     var loading = mutableStateOf(false)
+    var hasNextPage = mutableStateOf(false)
+    var search = mutableStateOf("")
 
+    init {
+        search.value = ""
+    }
     fun loadCustomers() {
         viewModelScope.launch {
             loading.value = true
-            // Thread.sleep(10_000)
-            Log.d("Customer view model", "loadCustomers")
-            val response = repository.getCustomers(page = currentPage)
+            Log.d("Customer view model", "loadCustomers CURRENT PAGE: $currentPage")
+
+            var customerSearchable: String? = null
+            if (search.value.length >= 3) {
+                currentPage.value = 1
+                customerSearchable = search.value
+            }
+
+            val response = repository.getCustomers(
+                page = currentPage.value,
+                search = customerSearchable
+            )
             when (response) {
                 is Resource.Success -> {
-                    Log.d("Customer view model", "loadCustomers - SUCCESS")
-                    val customerEntries = response.data!!.data.data.mapIndexed { _, entry ->
+                    val responsePaginated = response.data!!.data
+
+                    Log.d("Sucesso response paginated page", responsePaginated.page.toString())
+                    Log.d("Sucesso response paginated total", responsePaginated.total.toString())
+                    Log.d("Sucesso response paginated totalPages", responsePaginated.totalPages.toString())
+                    Log.d("Sucesso response paginated pageSize", responsePaginated.pageSize.toString())
+
+                    val customerEntries = responsePaginated.data.mapIndexed { _, entry ->
                         Customer(
                             id = entry.ID,
                             name = entry.Name,
@@ -37,16 +57,22 @@ class CustomerViewModel (
                             address = entry.Address
                         )
                     }
-                    // TODO: pagination
-                    //currentPage++
+                    currentPage.value = responsePaginated.page
                     error.value = ""
-                    customers.value = customerEntries
+                    if (currentPage.value > 1) {
+                        customers.value += customerEntries
+                    } else {
+                        customers.value = customerEntries
+                    }
                     loading.value = false
+                    hasNextPage.value = responsePaginated.totalPages > responsePaginated.page
                 }
                 is Resource.Error -> {
                     Log.d("Customer view model", "loadCustomers - ERROR: ${response.message}")
+                    currentPage.value = 1
                     loading.value = false
                     error.value = response.message.toString()
+                    hasNextPage.value = false
                 }
             }
         }
