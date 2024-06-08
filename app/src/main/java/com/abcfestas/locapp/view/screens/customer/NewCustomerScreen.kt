72617f1986b6a742029bb5ec974dd2edad06c121
Bruno Roboredo <vital.bcroboredo@terceiros.mag.com.br>
2024-06-08 17:22:33 +0100

    refactoring new customer layout and fixing save button on the bottom again

--- app/src/main/java/com/abcfestas/locapp/view/screens/customer/NewCustomerScreen.kt
+++ app/src/main/java/com/abcfestas/locapp/view/screens/customer/NewCustomerScreen.kt
@@ -16,10 +16,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -32,7 +28,6 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.abcfestas.locapp.LocAppApplication
 import com.abcfestas.locapp.R
-import com.abcfestas.locapp.data.models.Customer
 import com.abcfestas.locapp.ui.theme.Red
 import com.abcfestas.locapp.view.components.Button
 import com.abcfestas.locapp.view.screens.product.WizardFormTopNavigation
@@ -41,9 +36,26 @@ import com.abcfestas.locapp.viewmodel.viewModelFactory
 
 @Composable
 fun NewCustomerScreen (
-    navController: NavController
+    navController: NavController,
+    viewModel: NewCustomerViewModel = viewModel(factory = viewModelFactory {
+        NewCustomerViewModel(LocAppApplication.appModule.customerRepository)
+    })
 ) {
-    var customer by remember { mutableStateOf<Customer?>(null) }
+    val formState = viewModel.state
+    val context = LocalContext.current
+    val successMessage = stringResource(id = R.string.customer_created_successfully)
+
+    // Coletar os eventos do ViewModel
+    LaunchedEffect(Unit) {
+        viewModel._validationEvents.collect { event ->
+            when(event) {
+                is NewCustomerViewModel.ValidationEvent.Success -> {
+                    Toast.makeText(context, successMessage, Toast.LENGTH_LONG).show()
+                    navController.popBackStack()
+                }
+            }
+        }
+    }
 
     Box(
         modifier = Modifier.fillMaxSize()
@@ -68,129 +80,104 @@ fun NewCustomerScreen (
                     .verticalScroll(rememberScrollState())
                     .weight(1f, false)
             ) {
-                NewCustomerForm(navController = navController)
-            }
-        }
-    }
-}
-
-@Composable
-fun NewCustomerForm(
-    navController: NavController,
-    viewModel: NewCustomerViewModel = viewModel(factory = viewModelFactory {
-        NewCustomerViewModel(LocAppApplication.appModule.customerRepository)
-    })
-) {
-    val formState = viewModel.state
-    val context = LocalContext.current
-    val successMessage = stringResource(id = R.string.customer_created_successfully)
-
-    // Coletar os eventos do ViewModel
-    LaunchedEffect(Unit) {
-        viewModel._validationEvents.collect { event ->
-            when(event) {
-                is NewCustomerViewModel.ValidationEvent.Success -> {
-                    Toast.makeText(context, successMessage, Toast.LENGTH_LONG).show()
-                    navController.popBackStack()
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = formState.name,
+                    onValueChange = {
+                        viewModel.onEvent(NewCustomerFormEvent.NameChanged(it))
+                    },
+                    isError = formState.nameError != null,
+                    placeholder = {
+                        Text(text = "Nome")
+                    }
+                )
+                if (formState.nameError != null) {
+                    Text(text = formState.nameError, color = Red)
                 }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = formState.phone,
+                    onValueChange = {
+                        viewModel.onEvent(NewCustomerFormEvent.PhoneChanged(it))
+                    },
+                    isError = formState.phoneError != null,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    placeholder = {
+                        Text(text = "Telefone")
+                    }
+                )
+                if (formState.phoneError != null) {
+                    Text(text = formState.phoneError, color = Red)
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = formState.email,
+                    onValueChange = {
+                        viewModel.onEvent(NewCustomerFormEvent.EmailChanged(it))
+                    },
+                    isError = formState.emailError != null,
+                    placeholder = {
+                        Text(text = "E-mail")
+                    }
+                )
+                if (formState.emailError != null) {
+                    Text(text = formState.emailError, color = Red)
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = formState.address,
+                    onValueChange = {
+                        viewModel.onEvent(NewCustomerFormEvent.AddressChanged(it))
+                    },
+                    singleLine = false,
+                    minLines = 5,
+                    isError = formState.addressError != null,
+                    placeholder = {
+                        Text(text = "Endereço")
+                    }
+                )
+                if (formState.addressError != null) {
+                    Text(text = formState.addressError, color = Red)
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = formState.notes,
+                    onValueChange = {
+                        viewModel.onEvent(NewCustomerFormEvent.NotesChanged(it))
+                    },
+                    singleLine = false,
+                    minLines = 5,
+                    isError = formState.notesError != null,
+                    placeholder = {
+                        Text(text = "Notas")
+                    }
+                )
+                if (formState.notesError != null) {
+                    Text(text = formState.notesError, color = Red)
+                }
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
-    }
 
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = formState.name,
-        onValueChange = {
-            viewModel.onEvent(NewCustomerFormEvent.NameChanged(it))
-        },
-        isError = formState.nameError != null,
-        placeholder = {
-            Text(text = "Nome")
-        }
-    )
-    if (formState.nameError != null) {
-        Text(text = formState.nameError, color = Red)
+        Button(
+            label = stringResource(id = R.string.save),
+            onClick = {
+                viewModel.onEvent(NewCustomerFormEvent.Submit)
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomCenter)
+        )
     }
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = formState.phone,
-        onValueChange = {
-            viewModel.onEvent(NewCustomerFormEvent.PhoneChanged(it))
-        },
-        isError = formState.phoneError != null,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        placeholder = {
-            Text(text = "Telefone")
-        }
-    )
-    if (formState.phoneError != null) {
-        Text(text = formState.phoneError, color = Red)
-    }
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = formState.email,
-        onValueChange = {
-            viewModel.onEvent(NewCustomerFormEvent.EmailChanged(it))
-        },
-        isError = formState.emailError != null,
-        placeholder = {
-            Text(text = "E-mail")
-        }
-    )
-    if (formState.emailError != null) {
-        Text(text = formState.emailError, color = Red)
-    }
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = formState.address,
-        onValueChange = {
-            viewModel.onEvent(NewCustomerFormEvent.AddressChanged(it))
-        },
-        singleLine = false,
-        minLines = 5,
-        isError = formState.addressError != null,
-        placeholder = {
-            Text(text = "Endereço")
-        }
-    )
-    if (formState.addressError != null) {
-        Text(text = formState.addressError, color = Red)
-    }
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = formState.notes,
-        onValueChange = {
-            viewModel.onEvent(NewCustomerFormEvent.NotesChanged(it))
-        },
-        singleLine = false,
-        minLines = 5,
-        isError = formState.notesError != null,
-        placeholder = {
-            Text(text = "Notas")
-        }
-    )
-    if (formState.notesError != null) {
-        Text(text = formState.notesError, color = Red)
-    }
-    Spacer(modifier = Modifier.height(16.dp))
-
-    Button(
-        label = stringResource(id = R.string.save),
-        onClick = {
-          viewModel.onEvent(NewCustomerFormEvent.Submit)
-        },
-        modifier = Modifier.padding(16.dp)
-    )
 }
-
 
 @Preview(showBackground = true)
 @Composable
